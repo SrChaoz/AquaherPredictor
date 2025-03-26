48c636907ed27206f4d7263edb0cc9943485e32a
SrChaoz <mateorubencho@gmail.com>
2025-03-26 11:10:37 -0500

    Update Script and Backup - 26/03/2025 - 11:07

--- database/scripts/CanalMesiasDB.sql
+++ database/scripts/CanalMesiasDB.sql
@@ -57,3 +57,51 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+--4.Crear nueva tabla para datos predichos
+CREATE TABLE datos_predic (
+    id SERIAL PRIMARY KEY,  
+    fecha DATE,
+    ph DOUBLE PRECISION,
+    turbidez DOUBLE PRECISION,
+    conductividad DOUBLE PRECISION,
+    tds DOUBLE PRECISION,
+    dureza DOUBLE PRECISION,
+    color DOUBLE PRECISION,
+    ica DOUBLE PRECISION
+);
+
+SELECT * FROM datos_predic
+
+--5. Crar funcion para ingresar datos a la tabla:
+CREATE OR REPLACE FUNCTION insertar_datos_predic(
+  p_fecha DATE,
+  p_ph DOUBLE PRECISION,
+  p_turbidez DOUBLE PRECISION,
+  p_conductividad DOUBLE PRECISION,
+  p_tds DOUBLE PRECISION,
+  p_dureza DOUBLE PRECISION,
+  p_color DOUBLE PRECISION,
+  p_ica DOUBLE PRECISION
+)
+RETURNS VOID AS $$
+BEGIN
+  INSERT INTO datos_predic (
+    fecha, ph, turbidez, conductividad, tds, dureza, color, ica
+  )
+  VALUES (
+    p_fecha, p_ph, p_turbidez, p_conductividad,
+    p_tds, p_dureza, p_color, p_ica
+  );
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+
+
+
+
+
+
+
